9ec878433194b2772c9bb5aeb912c175d33c6272
thrlstl <matthewsteeleonline@gmail.com>
2021-04-23 17:16:36 -0500

    Footer Mobile Responsive

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -4,7 +4,10 @@ import MenuIcon from '../assets/images/hamburger-menu-icon.png'
 function MenuButton() {
     return(
         <div className='menu-button-container'>
-            <img className='menu-button' src={MenuIcon}></img>
+            <img 
+                className='menu-button' 
+                src={MenuIcon}
+                alt='menu-icon'></img>
         </div>
     )
 }
