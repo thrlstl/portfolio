5d5989b6704d37e2e88724c94b2894493afdfb15
thrlstl <matthewsteeleonline@gmail.com>
2021-04-22 22:44:08 -0500

    Initial implementation of light/dark mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react'
 import './scss/main.scss'
 import Header from './components/Header'
 
 function App() {
+
+  const [isLight, setIsLight] = useState(false)
+  const [theme, setTheme] = useState('dark')
+
+  useEffect(() => {
+    setTheme(() => {
+      return isLight
+        ? 'light'
+        : 'dark'
+    })
+  }, [isLight])
+
   return (
-    <div className="App">
+    <div theme={theme} className="App">
       <Header />
     </div>
   );
