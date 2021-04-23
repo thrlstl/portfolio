b1c7dfa936d637c0dc4ac684610f0a26c48e6716
thrlstl <matthewsteeleonline@gmail.com>
2021-04-23 13:58:06 -0500

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
 import './scss/main.scss'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import ThemeContext from './contexts/ThemeContext'
 
 function App() {
 
-  const [isLight, setIsLight] = useState(true)
+  const [isLight, setIsLight] = useState(false)
   const [theme, setTheme] = useState('dark')
 
   useEffect(() => {
@@ -20,6 +21,7 @@ function App() {
     <ThemeContext.Provider value={theme}>
       <div className="App" theme={theme}>
         <Header />
+        <Footer />
       </div>
     </ThemeContext.Provider>
   );
