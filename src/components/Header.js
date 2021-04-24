0119e7c7e4a8b1fb87d94324f4f8810f58b908bb
thrlstl <matthewsteeleonline@gmail.com>
2021-04-23 20:42:37 -0500

    header text, icon, bar responsivity improved

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,13 +10,13 @@ function Header() {
         <div theme={theme} className='header'>
             <img alt='header-icon' className='header-icon' src={headerIcon}></img>
             <div className='header-content'>
-                <text className='header-name'>Matthew Steele</text>
-                <text className='header-separator'>/</text>
-                <text className='header-title'>Software Engineer</text>
+                    <span className='header-name'>Matthew Steele</span>
+                    <span className='header-separator'>/</span>
+                    <span className='header-title'>Software Engineer</span>
             </div>
-            <div className='links-container'>
+            {/* <div className='links-container'>
                 <img alt='tbd' className='icon'></img>
-            </div>
+            </div> */}
         </div>
     )
 }
