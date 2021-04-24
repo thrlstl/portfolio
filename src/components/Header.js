f66f824cc6f4c807c2c49088fac9019b1cf47dcc
thrlstl <matthewsteeleonline@gmail.com>
2021-04-23 20:49:18 -0500

    Resolving comments error with Netlify build.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,14 @@ import ThemeContext from '../contexts/ThemeContext'
 function Header() {
 
     const theme = useContext(ThemeContext)
+    const separator = '//'
 
     return (
         <div theme={theme} className='header'>
             <img alt='header-icon' className='header-icon' src={headerIcon}></img>
             <div className='header-content'>
                     <span className='header-name'>Matthew Steele</span>
-                    <span className='header-separator'>/</span>
+                    <span className='header-separator'>{separator}</span>
                     <span className='header-title'>Software Engineer</span>
             </div>
             {/* <div className='links-container'>
