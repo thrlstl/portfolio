f8832f24e6ffcf490dde34379bd1d83af13f0ea9
thrlstl <matthewsteeleonline@gmail.com>
2021-04-25 17:30:23 -0500

    Resolving Footer Menu Netlify Errors

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -3,10 +3,11 @@ import React from 'react'
 function FooterMenu(props) {
 
     const animationType = props.animationType
+    const className = 'footer-menu' + ' ' + animationType
 
     return(
         <div 
-        className={'footer-menu' + ' ' + animationType}>
+        className={className}>
         </div>
     )
 }
