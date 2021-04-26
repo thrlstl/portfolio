10458fb7f7e01e09b52ce6562912ef34faa25368
thrlstl <matthewsteeleonline@gmail.com>
2021-04-25 22:56:44 -0500

    Splash Screen Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,17 @@
 import React, { useContext } from 'react'
 import CustomIcon from './CustomIcon'
 import ThemeContext from '../contexts/ThemeContext'
-// import isMobileContext from '../contexts/isMobileContext'
+import isMobileContext from '../contexts/isMobileContext'
 
 function Header() {
 
     const theme = useContext(ThemeContext)
-    // const isMobile = useContext(isMobileContext)
+    const isMobile = useContext(isMobileContext)
     const separator = '//'
 
     return (
         <div className='header' theme={theme}>
-            {/* {!isMobile ? <CustomIcon /> : null} */}
-            <CustomIcon />
+            {!isMobile ? <CustomIcon /> : null}
             <div className='header-content'>
                 <span className='header-text-container'>
                     <span className='header-name'>Matthew Steele</span>
