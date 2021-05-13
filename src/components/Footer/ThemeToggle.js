d5fba6fc1ab63425f20ab3457ff397be33da8bb7
thrlstl <matthewsteeleonline@gmail.com>
2021-05-13 17:28:30 -0500

    Preparing for Netlify deploy

--- src/components/Footer/ThemeToggle.js
+++ src/components/Footer/ThemeToggle.js
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
 import ThemeContext from '../../contexts/ThemeContext'
-import isTabletOrMobile from '../../contexts/isTabletOrMobileContext'
 import IconDark from '../../assets/images/theme-toggle/moon-icon.png'
 import IconLight from '../../assets/images/theme-toggle/sun-icon.png'
 import isTabletOrMobileContext from '../../contexts/isTabletOrMobileContext'
@@ -10,7 +9,6 @@ function ThemeToggle() {
     const { theme, setTheme } = useContext(ThemeContext)
     const isTabletOrMobile = useContext(isTabletOrMobileContext)
     const [iconType, setIconType] = useState(null)
-    const [animationClass, setAnimationClass] = useState('')
     const [styleType, setStyleType] = useState(null)
 
     useEffect(() => {
