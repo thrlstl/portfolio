c54dc9f9e93ea4c5a4c577a9b0da6e392598346f
thrlstl <matthewsteeleonline@gmail.com>
2021-05-15 14:40:16 -0500

    Project feed completed with theme colors updated

--- src/components/pages/projects-page/ProjectCard.js
+++ src/components/pages/projects-page/ProjectCard.js
@@ -1,12 +1,18 @@
+import React, { useContext } from 'react'
+import ThemeContext from '../../../contexts/ThemeContext'
+
 function ProjectCard(props) {
 
     const name = props.name
+    const { theme } = useContext(ThemeContext)
 
     return(
-        <div className='project-card-container'>
+        <div className='project-card-container' theme={theme}>
+            <div className='project-card-header'></div>
             <div className='project-card'>
 
             </div>
+            <div className='project-card-shadow-1'></div>
         </div>
     )
 }
