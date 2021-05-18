49e498cd2799b2454692934be926a813ea0db411
thrlstl <matthewsteeleonline@gmail.com>
2021-05-18 18:09:29 -0500

    Projects grid completed with mobile responsiveness

--- src/components/CustomIcon.js
+++ src/components/CustomIcon.js
@@ -5,14 +5,14 @@ import iconRight from '../assets/images/icon/icon-right-triangle.png'
 import iconConnector from '../assets/images/icon/icon-triangle-connector.png'
 
 function CustomIcon() {
-    
+
     let history = useHistory()
-    
+
     const handleClick = () => {
         history.push('/')
     }
 
-    return(
+    return (
         <div className='icon-container' onClick={() => handleClick()}>
             <img className='icon-connector' src={iconConnector} alt='icon-connector'></img>
             <img className='icon-left' src={iconLeft} alt='icon-left'></img>
