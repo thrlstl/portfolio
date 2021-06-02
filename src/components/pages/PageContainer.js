fb1956c708f2ceb1662c052e5f1ce16ecf6f4099
thrlstl <matthewsteeleonline@gmail.com>
2021-06-01 22:19:43 -0500

    Resolving scroll position error

--- src/components/pages/PageContainer.js
+++ src/components/pages/PageContainer.js
@@ -3,22 +3,15 @@ import { useLocation } from 'react-router-dom'
 
 function PageContainer(props) {
 
-    const { location, pathname } = useLocation()
+    const { pathname } = useLocation()
     const currentRoute = pathname.split('/')[1]
-    // const homePage = 'about'
     const pageContainer = document.querySelector('.page-container')
 
     useEffect(() => {
         return pageContainer
         ? pageContainer.scrollTo(0, 0)
         : null
-    }, [location, pageContainer])
-
-    // const currentPage = () => {
-    //     return currentRoute === ''
-    //     ? homePage
-    //     : currentRoute
-    // }
+    }, [pathname, pageContainer])
 
     return(
         <div className={`${currentRoute} page-container`}>
